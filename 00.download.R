f5fd43028e2e921b50c74a1a56b7ae296f6a6fdc
awqx <alisonwxin@gmail.com>
2017-07-12 14:32:04 -0400

    Clarified some code, removed unnecessary variables

--- 00.download.R
+++ 00.download.R
@@ -1,6 +1,7 @@
 # Libraries ---------------------------------------------------------------
-library(XML)   # Import XML files
+library(data.table)
 library(RCurl) # Read webpages with special access requirements
+library(XML)   # Import XML files
 
 
 # Download from ACS -------------------------------------------------------
@@ -9,7 +10,13 @@ library(RCurl) # Read webpages with special access requirements
 # http://stackoverflow.com/questions/3616406/
 # object-moved-error-in-using-the-rcurl-geturl-function-in-order-to-access-an-as
 
+# ~~~~ Rekharsly and Inoue Data ~~~~
+
+# Change the working directory location at your discretion
+setwd("~/SREP LAB/qsar")
 url           <- "http://pubs.acs.org/doi/full/10.1021/cr970015o"
+# The success of this step depends on access to the file
+# A paywall will cause this to crash. 
 file          <-
   getURL(url, .opts = curlOptions(followlocation = TRUE, cookiefile = "nosuchfile"))
 ri.table.list <-
@@ -25,3 +32,17 @@ dwnld.dir <- "./downloaded/"
 dir.create(dwnld.dir)
 save(ri.table.list, file = paste0(dwnld.dir, "ri.table.list.RData"))
 saveRDS(ri.table.list, file = paste0(dwnld.dir, "ri.table.list.RDS"))
+
+# ~~~~ Suzuki Data ~~~~
+
+url   <- "http://pubs.acs.org/doi/full/10.1021/ci010295f"
+file  <-
+  getURL(url, .opts = curlOptions(followlocation = TRUE, cookiefile = "nosuchfile"))
+suzuki.list <-
+  readHTMLTable(
+    file,
+    header = T,
+    as.data.frame = T,
+    stringAsFactors = F
+  )
+saveRDS(suzuki.list, paste0(dwnld.dir, "suzuki.list.RDS"))
