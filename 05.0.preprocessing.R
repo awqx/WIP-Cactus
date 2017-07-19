179ff0682a9d1984f91606a897744dfd1217c2ea
awqx <alisonwxin@gmail.com>
2017-07-19 15:52:50 -0400

    Re-working of PaDEL descriptor to minimize error

--- 05.0.preprocessing.R
+++ 05.0.preprocessing.R
@@ -9,7 +9,7 @@ library(tidyverse)
 # Pre-processing and cleaning ---------------------------------------------
 
 setwd("~/SREP LAB/qsar")
-padel <- readRDS("./molecules/descriptors/04.all.padel.RDS")
+padel <- readRDS("./molecules/descriptors/all.padel.RDS") # prev: 04.all.padel
 # Removing predictors with near zero variance - 1302 predictors
 zero.pred <- nearZeroVar(padel)
 zero.pred.names <- colnames(padel)[zero.pred]
@@ -21,47 +21,62 @@ padel.cd <- padel.no.zero %>%
   mutate(beta = ifelse(str_detect(host, "beta"), 1, 0)) %>%
   mutate(gamma = ifelse(str_detect(host, "gamma"), 1, 0))
 
-# Separating nnon-predictors from the data
-padel.split1 <- padel.cd %>% dplyr::select(., guest:data.source)
-padel.split2 <- padel.cd %>% dplyr::select(., -guest:-data.source)
+# Separating non-predictors from data
+padel.split1 <- padel.cd %>% dplyr::select(., guest:data.source) # %>%
+  # filter(data.source == "rekharsky.inoue")
+padel.split2 <- padel.cd %>% # filter(data.source == "rekharsky.inoue") %>% 
+  dplyr::select(., -guest:-data.source)
+
+padel.split3 <- padel.cd %>% dplyr::select(., guest:data.source) %>%
+  filter(data.source == "suzuki")
+padel.split4 <- padel.cd %>% filter(data.source == "suzuki") %>% 
+  dplyr::select(., -guest:-data.source)
+  
 
 # padel.pp = padel.preprocess, just shortened
-padel.pp <- preProcess(padel.split2, na.remove = T, 
+padel.temp <- do.call(data.frame,lapply(padel.split2, 
+                                        function(x) replace(x, is.infinite(x),NA)))
+padel.pp <- preProcess(padel.temp, na.remove = T, 
                   method = c("knnImpute", "center", "scale")) %>%
-  predict(., padel.split2)
+  predict(., padel.temp)
+
 # padel.pp1 <- preProcess(padel.pp, method = "BoxCox") %>%
 #   predict(., padel.pp) # Not sure if any effect from BoxCox
 
 # More nearZeroVar analysis after preprocessing, to be safe
-zero.pred2 <- nearZeroVar(padel.pp)[1:24] # Originally 25, but I want to retain
-                                          # gamma as a variable for binning
-zero.pred2.names <- colnames(padel.pp)[zero.pred2]
+zero.pred2 <- nearZeroVar(padel.pp)
+zero.pred2.names <- colnames(padel.pp)[zero.pred2]  # Originally gmin and gamma, 
+                                                    # but I wanted to keep gamma
+zero.pred2 <- zero.pred2[1]
 padel.pp <- padel.pp[ , -zero.pred2]
 
 # Removing highly correlated predictors
 too.high <- findCorrelation(cor(padel.pp), 0.95) # 0.95 mostly arbitrary
 corr <- names(padel.pp)[too.high]
 padel.pp <- padel.pp[ , -too.high]
+
 padel.pp <- cbind(padel.split1, padel.pp)
+
 colnames(padel.pp) <- str_replace(colnames(padel.pp), "-", ".")
 
-# Creating External Validation Set ----------------------------------------
+#     Creating External Validation Set ----------------------------------------
 
 set.seed(4)
 ext.val.ind <- sample(x = 1:nrow(padel.pp), 
                       size = round(0.15 * nrow(padel.pp)))
 ext.val <- padel.pp[ext.val.ind, ]
-
-# Data Organization and Saving --------------------------------------------
+padel.pp.all <- padel.pp
+padel.pp <- padel.pp[-ext.val.ind, ]
+#     Data Organization and Saving --------------------------------------------
 
 sprse.padel <- sparse.model.matrix(~., padel.pp)
 mat.padel <- as.matrix(padel.pp)
 
-saveRDS(padel.pp, "./padel.pp.RDS")
+saveRDS(padel.pp, "./padel.pp.new.RDS")
 saveRDS(sprse.padel, "./sprse.padel.RDS")
 saveRDS(mat.padel, "./mat.padel.RDS")
 
-saveRDS(ext.val, "./external validation set.RDS")
+saveRDS(ext.val, "./external validation set new.RDS")
 
 # Processing GAFS Data ----------------------------------------------------
 
@@ -92,3 +107,50 @@ saveRDS(sprse.ga, "./feature.select/sprse.ga.RDS")
 saveRDS(mat.ga, "./feature.select/mat.ga.RDS")
 
 saveRDS(ga.extval, "./feature.select/gafs.extval.RDS")
+
+
+# Processing 2D Only Data -------------------------------------------------
+
+twod <- readRDS("./molecules/descriptors/04.all.2d.RDS")
+# Removing predictors with near zero variance - 1302 predictors
+zero.pred <- nearZeroVar(twod)
+zero.pred.names <- colnames(twod)[zero.pred]
+twod.no.zero <- twod[ , -zero.pred]
+
+# Binning alpha, beta, and gamma
+twod.cd <- twod.no.zero %>% 
+  mutate(alpha = ifelse(str_detect(host, "alpha"), 1, 0)) %>%
+  mutate(beta = ifelse(str_detect(host, "beta"), 1, 0)) %>%
+  mutate(gamma = ifelse(str_detect(host, "gamma"), 1, 0))
+
+# Separating non-predictors from data
+twod.split1 <- twod.cd %>% dplyr::select(., guest:data.source)
+twod.split2 <- twod.cd %>% dplyr::select(., -guest:-data.source)
+
+# twod.pp = twod.preprocess, just shortened
+twod.pp <- preProcess(twod.split2, na.remove = T, 
+                       method = c("medianImpute", "center", "scale")) %>%
+  predict(., twod.split2)
+twod.pp <- lapply(twod.pp, as.numeric) %>% data.frame()
+twod.pp <- preProcess(twod.pp, na.remove = T, 
+                      method = c("medianImpute")) %>%
+  predict(., twod.pp)
+
+# Removing highly correlated predictors
+too.high <- findCorrelation(cor(twod.pp), 0.95) # 0.95 mostly arbitrary
+corr <- names(twod.pp)[too.high]
+twod.pp <- twod.pp[ , -too.high]
+twod.pp <- cbind(twod.split1, twod.pp)
+
+colnames(twod.pp) <- str_replace(colnames(twod.pp), "-", ".")
+
+#     External Validation ----
+set.seed(4)
+ext.val.ind <- sample(x = 1:nrow(twod.pp), 
+                      size = round(0.15 * nrow(twod.pp)))
+ext.val <- twod.pp[ext.val.ind, ]
+
+#     Data Organization and Saving --------------------------------------------
+
+saveRDS(twod.pp, "./2d.pp.RDS")
+saveRDS(ext.val, "./2d.extval.RDS")
--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Status
 ## Current Issues
 1. Updating of all models with new data
-2. Feature selection with GF and PLS for combined data
+2. Tuning of SVM
 
 ## Most Recent Additions
-1. Incorporation of Suzuki into SDF download
-2. Download of SDFs within R as well as externally
-3. Merging of Rekharsky/Inoue and Suzuki data
-4. Combining experimental results with PaDEL-Descriptor predictors
+1. GAFS on Suzki and Rekharsky/Inoue data
+2. Fixing download cactus for modelable data (previous function created sparse SDFs)
 
 ## Near Future Possibilities
 1. Chemical descriptor calculation within R with Rcdk
