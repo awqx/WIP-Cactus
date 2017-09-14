efcbb598a2cdd3d605959ba606dc639db444e24b
awqx <alisonwxin@gmail.com>
2017-09-14 16:21:58 -0400

    initial commit

---
+++ 06.tropsha.R
@@ -0,0 +1,17 @@
+# Models evaluated by standards from Tropsha 2010, DOI: 10.1002/minf201000061
+# "Best Practices for QSAR Model Development, Validation, and Exploitation"
+
+# Set working directory as needed
+# setwd("~/SREP LAB/qsar")
+
+# Libraries and Packages --------------------------------------------------
+
+library(tidyverse)
+library(caret)
+
+# Importing Models --------------------------------------------------------
+
+
+# Evaluating Standards ----------------------------------------------------
+
+
