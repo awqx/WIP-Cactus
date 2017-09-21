f0b75def77926f1f022cfa933b70a7b4602df257
awqx <alisonwxin@gmail.com>
2017-09-21 14:54:46 -0400

    Evaluation completed as according to Xu 2015

--- 06.tropsha.R
+++ 06.tropsha.R
@@ -21,19 +21,37 @@ find.k <- function(results) {
   return (top/bottom)
 }
 
+# Equation writeen in Xu2015 unclear
+find.kprime <- function(results) {
+  top <- sum(results$pred * results$obs)
+  bottom <- sum(results$pred ^ 2)
+  return (top/bottom)
+}
+
 find.q2.f1 <- function(results) {
   top <- (results$obs - results$pred)^2 %>% sum()
   bottom <- (results$obs - mean(results$pred))^2 %>% sum()
   return (1 - (top/bottom))
 }
 
-find.r2r20 <- function(results) {
+# Uses caret::defaultSummary
+find.r2 <- function(results) {
+  return(defaultSummary(results)[2])
+}
+
+find.r20 <- function(results) {
   ybarr0 <- find.k(results) * results$obs
-  top <- (results$obs - ybarr0)^2 %>% sum()
+  top <- (results$pred - ybarr0)^2 %>% sum()
+  bottom <- (results$pred - mean(results$pred))^2 %>% sum()
+  return(1 - (top/bottom))
+}
+
+# May be wrong due to reasons specified in find.kprime
+find.r20prime <- function(results) {
+  yr0 <- find.kprime(results) * results$pred
+  top <- (results$obs - yr0)^2 %>% sum()
   bottom <- (results$obs - mean(results$obs))^2 %>% sum()
-  r20 <- 1 - (top/bottom)
-  r2 <- defaultSummary(results)[2]
-  return((r2-r20)/r2)
+  return(1 - (top/bottom))
 }
 
 # diff.r2r20 <- function(results) {
@@ -62,6 +80,13 @@ find.k(rf) # 0.965
 find.k(glm) # 1.002
 find.k(pls) # 0.951
 
+# 0.85 <= k-prime <- 1.15
+find.kprime(cube) # 0.972
+find.kprime(svm) # 0.996
+find.kprime(rf) # 0.982
+find.kprime(glm) # 0.956
+find.kprime(pls) # 1.009
+
 # Q2-F1 > 0.5
 find.q2.f1(cube) # 0.659
 find.q2.f1(svm) # 0.601
@@ -69,9 +94,16 @@ find.q2.f1(rf) #0.678
 find.q2.f1(glm) # 0.694
 find.q2.f1(pls) # 0.683
 
-# (R2-R02)/R2 < 0.1
-find.r2r20(cube) # -0.441
-find.r2r20(svm) # -0.601
-find.r2r20(rf) # -0.417
-find.r2r20(glm) # -0.412
-find.r2r20(pls) # -0.435
+# (R2-R20-prime)/R2 < 0.1
+(find.r2(cube) - find.r20prime(cube))/find.r2(cube) # 0.040
+(find.r2(svm) - find.r20prime(svm))/find.r2(svm)  # 0.020
+(find.r2(rf) - find.r20prime(rf))/find.r2(rf)  # 0.032
+(find.r2(glm) - find.r20prime(glm))/find.r2(glm) # 0.009
+(find.r2(rf) - find.r20prime(pls))/find.r2(pls)  # 0.024
+
+# abs(R20- - R20prime) < 0.3
+abs(find.r20(cube) - find.r20prime(cube)) #0.007
+abs(find.r20(svm) - find.r20prime(svm)) # 0.080
+abs(find.r20(rf) - find.r20prime(rf)) #0.312
+abs(find.r20(glm) - find.r20prime(glm)) # 0.187
+abs(find.r20(pls) - find.r20prime(pls)) #0.113
