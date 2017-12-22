8a777d69e4ddc646d0c94b1b883a63affdb555b5
awqx <alisonwxin@gmail.com>
2017-12-22 15:13:57 -0500

    trial of diffusion calc

--- 08.vulic.R
+++ 08.vulic.R
@@ -63,3 +63,45 @@ tran.cylindrical(C = C, D.r = D, r = r, theta = theta, z = z,
 
 # zero-gradient boundary conditions
 tran.cylindrical(C = C, D.r = D, r = r, theta = theta, z = z)
+
+
+# Model w/ Diffusion and First-Order Consumption
+N     <- 50          # number of grid cells
+XX    <- 4           # total size
+rr    <- 0.005       # consumption rate
+ini   <- 1           # initial value at x=0
+D     <- 400
+r     <- seq (2, 4, len = N+1)
+theta   <- seq(0, 2*pi, len = N+1)
+theta.m <- 0.5*(theta[-1]+theta[-(N+1)])
+# The model equations
+Diffpolar <- function (t, y, parms)  {
+  CONC  <- matrix(nrow = N, ncol = N, data = y)
+  tran  <- tran.polar(CONC, D.r = D, D.theta = D, r = r, theta = theta,
+                      C.r.up = 0, C.r.down = 1*sin(5*theta.m),
+                      cyclicBnd = 2, full.output=TRUE )
+  dCONC <- tran$dC  - rr * CONC
+  return (list(dCONC))
+}
+# solve to steady-state; cyclicBnd = 2, because of C.theta.up, C.theta.down
+out <- steady.2D (y = rep(0, N*N), func = Diffpolar, parms = NULL,
+                  dim = c(N, N), lrw = 1e6, cyclicBnd = 2)
+image(out)
+cart <- polar2cart(out, r = r, theta = theta,
+                   x = seq(-4, 4, len = 100),
+                   y = seq(-4, 4, len = 100))
+image(cart)
+
+# Messing Around
+diff1 <- tran.cylindrical (C = C, D.r = D, D.theta = D, 
+                  C.r.up = 1, C.r.down = 1,
+                  C.theta.up = 1, C.theta.down = 1, 
+                  C.z.up = 1, C.z.down = 1,
+                  r = r, theta = theta, z = z )
+diff1.1 <- diff1[1] %>% unlist()
+diff1.1 <- array(dim = c(r.N, theta.N, z.N), data = diff1.1)
+diff2 <- tran.cylindrical (C = diff1.1, D.r = D, D.theta = D, 
+                           C.r.up = 1, C.r.down = 1,
+                           C.theta.up = 1, C.theta.down = 1, 
+                           C.z.up = 1, C.z.down = 1,
+                           r = r, theta = theta, z = z )
