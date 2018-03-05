39eded55355bacd7d5c49ec7dfa3544cb65291ac
awqx <alisonwxin@gmail.com>
2018-03-05 16:37:50 -0500

    fixing evaluation of some models, graph

--- graph.formatting.R
+++ graph.formatting.R
@@ -36,6 +36,7 @@ all <- transform(all.results, type = factor(
 
 # Graphs ------------------------------------------------------------------
 
+# For the poster
 dir.create("./graphs/2018 poster")
 
 plot.2018(docking) + 
@@ -72,3 +73,49 @@ plot.2018(all) +
   labs(title = "Docking vs. Various QSAR Predictions") + 
   geom_point(size = 0.5)
 ggsave("./graphs/2018 poster/2018-02-12 docking and qsar.png", dpi = 600, scale = 1.5)
+
+# For the manuscript, 2018
+dir.create("./graphs/2018 paper")
+
+plot.paper.2018(docking) 
+ggsave("./graphs/2018 paper/docking results.png")
+
+plot.paper.2018(qsar.results) +
+  facet_wrap(~qsar.type)
+ggsave("./graphs/2018 paper/qsar results.png", scale = 1.5)
+
+plot.paper.2018(all) + 
+  facet_wrap(~type) 
+ggsave("./graphs/2018 paper/docking and qsar.png", scale = 1.5)
+
+temp.all <- all %>% mutate(Data = "Test")
+ext.val <- readRDS("./ext val results.RDS") %>% mutate(Data = "External validation") %>%
+  rename(type = model)
+tst.ev.all <- rbind(temp.all, ext.val) %>%
+  filter(type != "PyRx Docking")
+
+# plot.paper.2018(tst.ev.all) + 
+#   facet_wrap(~type) + 
+#   geom_point(aes(shape = Data)) + 
+
+shapes <- c("16" = "Test", "0" = "External validation")  
+ggplot(tst.ev.all, aes(x = obs, y = pred, color = cd.type, shape = Data)) +
+  geom_point() + 
+  theme.paper.2018 + 
+  facet_wrap(~type) + 
+  coord_fixed(xlim = c(-45,5), ylim = c(-45, 5)) +
+  geom_abline(intercept = 0, slope = 1) + 
+  labs(x = "Experimental dG, kJ/mol", y = "Predicted dG, kJ/mol",
+       color = "CD Type") + 
+  scale_shape_manual(values = c(0,16)) 
+ggsave("./graphs/2018 paper/test and extval.png")
+
+qsar.type <- c("Cubist", "GLMNet", "PLS", "Random Forest", "SVM")
+qsar.time <- c(445.91, 4.314, 176.01, 635.8, 9.59)
+times <- data.frame(temp.types, qsar.times)
+
+ggplot(times, aes(x = qsar.type, y = qsar.time)) + 
+  geom_bar(stat = "identity") + 
+  theme.paper.2018 + 
+  labs(x = "QSAR type", y = "Time used, seconds")
+ggsave("./graphs/2018 paper/time.png", scale = 0.75)
--- graph.functions.R
+++ graph.functions.R
@@ -8,6 +8,7 @@ library(tidyverse)
 # Functions ---------------------------------------------------------------
 
 font_import(pattern = "[B/b]ahnschrift")
+font_import(pattern = "Average")
 loadfonts(device = "win")
 
 theme.2018 <- theme(
@@ -25,6 +26,27 @@ plot.2018 <- function(data) {
     geom_point() + 
     geom_abline(slope = 1, intercept = 0, color = "maroon") +
     theme.2018 + 
+    coord_fixed(xlim = c(-45,5), ylim = c(-45, 5)) +
+    labs(x = "Experimental dG, kJ/mol", y = "Predicted dG, kJ/mol",
+         color = "CD Type")
+}
+
+theme.paper.2018 <- theme(
+  plot.background = element_rect(fill = "white", color = NA), 
+  panel.grid.major = element_line(color = NA),
+  panel.background = element_rect(fill = "NA", color = "black"), 
+  panel.border = element_rect(fill = NA, color = "black"),
+  legend.background = element_rect(fill = "white", color = NA),
+  legend.key = element_rect(fill = "white", color = NA),
+  strip.background = element_rect(fill = "white", color = NA),
+  text = element_text(size = 16, family = "Bahnschrift")
+)
+
+plot.paper.2018 <- function(data) {
+  ggplot(data, aes(x = obs, y = pred, color = cd.type)) +
+    geom_abline(slope = 1, intercept = 0, color = "black") +
+    geom_point(size = 1) + 
+    theme.paper.2018 + 
     coord_fixed(xlim = c(-45,5), ylim = c(-45, 5)) +
     labs(x = "Experimental dG, kJ/mol", y = "Predicted dG, kJ/mol",
          color = "CD Type")
