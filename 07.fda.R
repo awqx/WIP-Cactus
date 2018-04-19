bbb47e07dbd981bca2941a77c07510847066458f
awqx <alisonwxin@gmail.com>
2018-04-19 16:05:57 -0400

    added analysis of the FDA-approved drugs from Drugbank including SDF cleanup, pre-processing, GLMNet, and SVM

--- 07.fda.R
+++ 07.fda.R
@@ -184,10 +184,10 @@ fda.padel <- rbind(fda.padel1, fda.padel2, fda.padel3, fda.padel3)
 # fda.gamma <- fda.padel %>% mutate(alpha = 0) %>%
 #   mutate(beta = 0) %>% mutate(gamma = 1)
 
-pp.settings <- readRDS("./preprocess/preprocess.settings.RDS")
-zero.pred <- readRDS("./preprocess/zero.pred.RDS") %>% str_replace(., "-", ".")
-zero.pred2 <- readRDS("./preprocess/zero.pred2.RDS")[1]
-too.high <- readRDS("./preprocess/high.cor.RDS") %>% str_replace(., "-", ".")
+pp.settings <- readRDS("./pre-process/pp.settings.RDS")
+zero.pred <- readRDS("./pre-process/zero.pred.RDS") %>% str_replace(., "-", ".")
+zero.pred2 <- readRDS("./pre-process/zero.pred2.RDS")[1]
+too.high <- readRDS("./pre-process/high.cor.RDS") %>% str_replace(., "-", ".")
 
 fda.desc <- fda.padel[ , !colnames(fda.padel) %in% zero.pred]
 
@@ -195,7 +195,7 @@ fda.desc <- fda.desc[ , -1]
 fda.desc <- rbind(fda.desc %>% mutate(alpha = 1, beta = 0, gamma = 0), 
                   fda.desc %>% mutate(alpha = 0, beta = 1, gamma = 0), 
                   fda.desc %>% mutate(alpha = 0, beta = 0, gamma = 1))
-fda.pp <- predict(pp.settings, fda.desc)
+fda.pp <- predict(pp.settings, fda.desc, na.remove = T)
 
 fda.pp <- fda.pp[ , !colnames(fda.pp) %in% too.high]
 fda.pp <- fda.pp[ , !colnames(fda.pp) %in% zero.pred2]
@@ -393,3 +393,190 @@ ggplot(fda.comb, aes(x = guest, y = pred, color = cd.type)) +
     axis.title.x = element_blank()
   ) 
 ggsave("./graphs/fda.png", dpi = 600)
+
+
+# CureFFI Data ------------------------------------------------------------
+
+dir.create("./fda")
+download.file(url = "http://www.cureffi.org/wp-content/uploads/2013/10/drugs.txt", 
+              destfile = "./fda/cureffi-fda.txt")
+cureffi <- read.csv("./fda/cureffi-fda.txt", header = T, 
+                    sep = "\t")
+fda <- cureffi %>% select(-cns_drug) %>%
+  filter(smiles != "")
+fda.test <- fda[1:5, ] 
+write.table(fda.test, file = "./fda/test.smi", 
+            col.names = T, row.names = F, quote = F, 
+            sep = "\t", eol = "\n")
+
+# DrugBank Data -----------------------------------------------------------
+
+drugbank <- read.csv("./drugbank/structure links.csv")
+drugbank.smi <- drugbank %>% 
+  filter(SMILES != "") %>%
+  select(SMILES) %>%
+  mutate(SMILES = as.character(SMILES))
+smi.small <- drugbank.smi %>%
+  filter(nchar(SMILES) < 30)
+small.names <- drugbank %>%
+  mutate(SMILES = as.character(SMILES)) %>%
+  filter(nchar(SMILES) < 30) %>%
+  select(Name) %>%
+  rename(guest = Name) %>%
+  as.vector()
+
+write.table(smi.small, "./drugbank/small-drugbank.SMI", quote = F, 
+            sep = "\t", row.names = F, col.names = F)
+
+write.table(drugbank.smi, "./drugbank/drugbank.SMI", quote = F, 
+            sep = "\t", col.names = F, row.names = F)
+
+# After running PaDEL
+db.small <- read.csv("./drugbank/small-drugbank.csv") %>%
+  mutate(Name = small.names) %>%
+  rename(guest = Name)
+
+
+#     Pre-processing ------------------------------------------------------
+
+pp.settings <- readRDS("./pre-process/pp.settings.RDS")
+zero.pred <- readRDS("./pre-process/zero.pred.RDS") %>% str_replace(., "-", ".")
+zero.pred2 <- readRDS("./pre-process/zero.pred2.RDS")[1]
+too.high <- readRDS("./pre-process/high.cor.RDS") %>% str_replace(., "-", ".")
+
+desc <- db.small[ , !colnames(db.small) %in% zero.pred]
+db.pp <- predict(pp.settings, desc)
+db.pp <- db.pp[ , !colnames(db.pp) %in% too.high] %>%
+  .[ , !colnames(db.pp) %in% zero.pred2]
+
+# Testing SDF splitting ---------------------------------------------------
+
+db.comp <- read.csv("./drugbank/structure-sample.SDF", 
+           header = F, sep = "\n")
+db.comp <- read.csv("./drugbank_approved_structures/structures.SDF", 
+                    header = F, sep = "\n")
+
+# index where SDF encoding ends, 
+# aka where the new SDF starts in the next line
+end.index <- which(db.comp$V1 == "$$$$") 
+# index where names should go
+name.index <- c(1, end.index[1:(length(end.index) - 1)] + 1) %>% as.integer()
+
+# index where generic names can be found
+generic.index <- which(db.comp$V1 == "> <GENERIC_NAME>") + 1
+generic.names <- db.comp$V1[generic.index] %>% as.character()
+
+# simple enough replacement at the correct indices
+db.comp$V1[name.index] <- generic.names
+
+# location where useful encoding ends
+mend.index <- which(db.comp$V1 == "M  END")
+
+subset.sdf <- function(name, mend, sdfs) {
+  result <- sdfs[name:mend, 1] 
+  header <- rep(" ", 3)
+  header.end <- result %>% as.character() %>% str_detect(., "V2000") %>% which(.)
+  header.temp <- result[1:(header.end - 1)]
+  result <- result[header.end:length(result)]
+  
+  for(i in 1:length(header.temp))
+    header[i] <- header.temp[i]
+  
+  result <- c(header, result, "$$$$")
+  result <- data.frame(result)
+  return(result)
+} 
+db.char <- db.comp %>% mutate(V1 = as.character(V1))
+do.call(rbind, mapply(FUN = subset.sdf, name = name.index, mend = mend.index, MoreArgs = list(sdfs = db.char)))
+temp <- mapply(FUN = subset.sdf, name = name.index, mend = mend.index, MoreArgs = list(sdfs = db.char), SIMPLIFY = F) %>% rbindlist()
+
+# write.table(temp, file = "./drugbank/fixed-sample.SDF", quote = F, row.names = F, col.names = F)
+write.table(temp, file = "./drugbank/all.SDF", quote = F, row.names = F, col.names = F)
+
+
+# extract.sdf <- function(sdf.df) { 
+#   
+#   }
+#   
+# Anyway, here's the CSV
+# Complications: can't use 3d coordinates or standardize tautomers
+fda.padel <- read.csv("./drugbank/all.csv")
+
+# Pre-processing
+pp.settings <- readRDS("./pre-process/pp.settings.RDS")
+zero.pred <- readRDS("./pre-process/zero.pred.RDS") %>% str_replace(., "-", ".")
+zero.pred2 <- readRDS("./pre-process/zero.pred2.RDS")[1]
+too.high <- readRDS("./pre-process/high.cor.RDS") %>% str_replace(., "-", ".")
+
+fda.desc <- fda.padel[ , !colnames(fda.padel) %in% zero.pred] 
+
+fda.desc <- fda.desc[complete.cases(fda.desc), ]
+
+fda.guest <- fda.desc[ , 1]
+fda.desc <- fda.desc[ , -1] 
+fda.desc <- rbind(fda.desc %>% mutate(alpha = 1, beta = 0, gamma = 0), 
+                  fda.desc %>% mutate(alpha = 0, beta = 1, gamma = 0), 
+                  fda.desc %>% mutate(alpha = 0, beta = 0, gamma = 1))
+
+fda.pp <- predict(pp.settings, fda.desc, na.ignore = T)
+
+fda.pp <- fda.pp[ , !colnames(fda.pp) %in% too.high]
+fda.pp <- fda.pp[ , !colnames(fda.pp) %in% zero.pred2]
+
+fda.data <- cbind(fda.guest, fda.pp)
+colnames(fda.data)[1] <- "guest"
+
+# GLMNet predictions on DrugBank ------------------------------------------
+
+glm.a <- readRDS("./models/glmnet/glm.alpha.RDS")
+
+# SVM predictions on DrugBank ---------------------------------------------
+
+svm.a <- readRDS("./models/svm/polysvm.alpha.RDS")
+svm.b <- readRDS("./models/svm/polysvm.beta.RDS")
+svm.c <- readRDS("./models/svm/polysvm.gamma.RDS")
+
+fda.svm.a <- predict(svm.a, fda.pp %>% filter(alpha > 0)) %>%
+  as.data.frame() %>% mutate(cd.type = "alpha")
+fda.svm.b <- predict(svm.b, fda.pp %>% filter(beta > 0)) %>%
+  as.data.frame() %>% mutate(cd.type = "beta")
+fda.svm.c <- predict(svm.c, fda.pp %>% filter(gamma > 0)) %>%
+  as.data.frame() %>% mutate(cd.type = "gamma")
+
+guest <- fda.data$guest %>% as.vector() %>% rep(., 3)
+fda.svm <- data.frame(guest, rbind(fda.svm.a, fda.svm.b, fda.svm.c)) %>%
+  rename(., pred = `.`)
+
+ggplot(fda.svm, aes(x = guest, y = pred, color = cd.type)) + 
+  geom_point() + 
+  theme_bw() + 
+  theme(axis.text.x = ) + 
+  coord_cartesian(ylim = c(-50, 50))
+
+# GLMNet ------------------------------------------------------------------
+
+glm.a <- readRDS("./models/glmnet/glm.alpha.RDS")
+glm.b <- readRDS("./models/glmnet/glm.beta.RDS")
+glm.c <- readRDS("./models/glmnet/glm.gamma.RDS")
+
+fda.a <- fda.pp %>% filter(alpha > 0) %>% as.matrix()
+fda.b <- fda.pp %>% filter(beta > 0) %>% as.matrix()
+fda.c <- fda.pp %>% filter(gamma > 0) %>% as.matrix()
+fda.glm.a <- predict.glmnet(glm.a, fda.a, s = tail(glm.a$lambda, n = 1)) %>%
+  as.data.frame() %>% mutate(cd.type = "alpha")
+fda.glm.b <- predict.glmnet(glm.b, fda.b, s = tail(glm.b$lambda, n = 1)) %>%
+  as.data.frame() %>% mutate(cd.type = "beta")
+fda.glm.c <- predict.glmnet(glm.c, fda.c, s = tail(glm.c$lambda, n = 1)) %>%
+  as.data.frame() %>% mutate(cd.type = "gamma")
+
+fda.glm <- data.frame(guest, rbind(fda.glm.a, fda.glm.b, fda.glm.c)) %>%
+  rename(., pred = X1)
+
+ggplot(fda.glm, aes(x = guest, y = pred, color = cd.type)) +
+  geom_point(alpha = 0.6) + 
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
+  
+labs(title = "GLMNet FDA Approved Drugs", 
+       y = "Predicted DelG, kJ/mol", 
+       color = "CD Type", shape = "Cyclodextrin")
