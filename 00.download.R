50033d522444d1525b16f2d91a4725dc0137ff45
awqx <alisonwxin@gmail.com>
2018-06-04 10:13:47 -0400

    Added download of new data from Singh 2015 (DOI  10.1039/C5RA21298G). May not actually use data in model-building due to suspicious Ka values.

--- 00.download.R
+++ 00.download.R
@@ -1,8 +1,11 @@
 # Libraries ---------------------------------------------------------------
-packages <- c("data.table", "RCurl", "XML")
+packages <- c("data.table", "RCurl", "tabulizer", "XML")
 mapply(install.packages, packages)
+
 library(data.table)
 library(RCurl) # Read webpages with special access requirements
+library(tabulizer) # read tables from PDFs
+library(tidyverse)
 library(XML)   # Import XML files
 
 # Download from ACS -------------------------------------------------------
@@ -32,7 +35,7 @@ ri.table.list <-
 dir.create("./dwnld")
 saveRDS(ri.table.list, file = "./dwnld/ri.table.list.RDS")
 
-# ---- Suzuki Data ----
+# Suzuki Data ----
 
 url   <- "http://pubs.acs.org/doi/full/10.1021/ci010295f"
 file  <-
@@ -48,3 +51,25 @@ suzuki.list <-
   )
 
 saveRDS(suzuki.list, "./dwnld/suzuki.list.RDS")
+
+#  Singh Data -------------------------------------------------------------
+
+convert.ka.delg <- function(ka) {
+  return(-8.314*298*log(ka)/1000)
+}
+
+# The file should be downloaded as a PDF at the filepath listed below
+singh.raw <- extract_tables("./dwnld/singh.pdf") %>% as.data.frame()
+singh1 <- singh.raw[[1]][-1, 1:4] %>% data.frame()
+colnames(singh1) <- c("guest", "alpha1", "beta1", "gamma1")
+singh1[ , 2:4] <- sapply(singh1[ , 2:4], as.character) %>% sapply(., as.numeric)
+singh2 <- singh.raw[[2]][-1, 1:4] %>% data.frame()
+colnames(singh2) <- c("guest", "alpha2", "beta2", "gamma2")
+singh2[ , 2:4] <- sapply(singh2[ , 2:4], as.character) %>% sapply(., as.numeric)
+singh <- inner_join(singh1, singh2) %>%
+  mutate(alpha = alpha1/alpha2, beta = beta1/beta2, gamma = gamma1/gamma2) %>%
+  select(guest, alpha, beta, gamma) 
+singh[ , 2:4] <- sapply(singh[ , 2:4], convert.ka.delg)
+
+saveRDS(singh, "./dwnld/singh.RDS")
+
