213ff2daabd33e21ac450189cc85b540ece1adda
awqx <alisonwxin@gmail.com>
2018-06-15 10:55:57 -0400

    Added tuning for PLS. Decent R-squared (~0.7) but still lower than others (GLMNet, RF, SVM)

--- 07.3.0.pls.tune.R
+++ 07.3.0.pls.tune.R
@@ -1,6 +1,6 @@
 # Libraries and Packages --------------------------------------------------
 
-install.packages("pls")
+# install.packages("pls")
 library(pls)
 library(tidyverse)
 
@@ -30,6 +30,7 @@ tune.pls.ncomp <- function(data, nfolds, comp, seed) {
     r2.results[i] <- defaultSummary(pls.df)[2]
     rmse.results[i] <- defaultSummary(pls.df)[1]
   }
+  # message(comp)
   
   return(data.frame( # Useful for records
     data = deparse(substitute(data)), # Turns the variable name into char
@@ -141,7 +142,7 @@ saveRDS(results.methods, "./tuning/pls/alpha/methods.RDS")
 
 #     Number of components ------------------------------------------------
 
-ncomp.range <- c(1, seq(5, 50, 9))
+ncomp.range <- c(1, 2, 5, 10, 20, 25)
 results1.ncomp <- do.call(rbind, lapply(ncomp.range, FUN = tune.pls.ncomp, 
                                           data = trn, nfolds = 10, seed = 101))
 results2.ncomp <- do.call(rbind, lapply(ncomp.range, FUN = tune.pls.ncomp, 
@@ -175,13 +176,16 @@ system.time(
 
 # system.time
 # user  system elapsed 
-# 14.26    0.07   14.71 
+# 3.89    0.00    3.93 
 
 results.combos[order(results.combos$rsquared, decreasing = T), ] %>% head()
 results.combos[order(results.combos$rmse), ] %>% head()
 
-# r2 = 0.442, rmse = 3.90
-# ncomp = 5, method = "kernelpls"
+# r2 = 0.712, rmse = 3.38
+# ncomp = 10, method = "kernelpls"
+
+# r2 = 0.662, rmse = 3.36
+# ncomp = 25. methpd = widekernelpls
 
 saveRDS(results.combos, "./tuning/pls/alpha/tune.RDS")
 results.combos <- results.combos %>%
@@ -193,3 +197,87 @@ ggplot(results.combos, aes(x = ncomp, y = method, fill = rsquared)) +
   labs(title = "GLMNet tuning for alpha-CD", x = "Alpha", y = "Maximum degrees of freedom", 
        fill = "R2")
 ggsave("./tuning/pls/alpha/tune.png", dpi = 450)
+
+# Beta -------------------------------------------------------------------
+
+#     Loading Data --------------------------------------------------------
+
+dir.create("./tuning/pls/beta")
+
+# Reading data with all descriptors
+trn.all <- readRDS("./pre-process/beta/1/pp.RDS") 
+colnames(trn.all) <- str_replace(colnames(trn.all), "-", ".")
+trn.guest <- trn.all$guest
+trn <- select(trn.all, -guest)
+
+rfe1 <- readRDS("./feature.selection/beta/rfe1.RDS")
+trn.pred <- c("DelG", predictors(rfe1))
+
+trn <- trn[ , colnames(trn) %in% trn.pred] 
+
+#     Method --------------------------------------------------------------
+
+method.range <- c("kernelpls", "simpls", "oscorespls", "widekernelpls")
+results1.methods <- do.call(rbind, lapply(method.range, FUN = tune.pls.method, 
+                                          data = trn, nfolds = 10, seed = 101))
+results2.methods <- do.call(rbind, lapply(method.range, FUN = tune.pls.method, 
+                                          data = trn, nfolds = 10, seed = 102))
+results.methods <- rbind(results1.methods, results2.methods)
+saveRDS(results.methods, "./tuning/pls/beta/methods.RDS")
+
+#     Number of components ------------------------------------------------
+
+ncomp.range <- c(1, 2, 3, 5, 10, 15)
+results1.ncomp <- do.call(rbind, lapply(ncomp.range, FUN = tune.pls.ncomp, 
+                                        data = trn, nfolds = 10, seed = 101))
+results2.ncomp <- do.call(rbind, lapply(ncomp.range, FUN = tune.pls.ncomp, 
+                                        data = trn, nfolds = 10, seed = 102))
+results3.ncomp <- do.call(rbind, lapply(ncomp.range, FUN = tune.pls.ncomp, 
+                                        data = trn, nfolds = 10, seed = 103))
+results.ncomp <- rbind(results1.ncomp, results2.ncomp, results3.ncomp)
+saveRDS(results.ncomp, "./tuning/pls/beta/ncomp.RDS")  
+
+#     Tuning --------------------------------------------------------------
+
+pls.combos <- expand.grid(as.character(method.range), ncomp.range)
+colnames(pls.combos) <- c("method", "ncomp")
+method.combos <- pls.combos$method %>% as.character()
+ncomp.combos <- pls.combos$ncomp
+
+set.seed(1001)
+system.time(
+  results.combos <- do.call(
+    rbind,
+    mapply(
+      FUN = tune.pls,
+      comp = ncomp.combos,
+      met = method.combos,
+      MoreArgs = 
+        list(nfolds = 10, data = trn), 
+      SIMPLIFY = F
+    )
+  )
+)
+
+# system.time
+#  user  system elapsed 
+# 1.47    0.00    1.50 
+
+results.combos[order(results.combos$rsquared, decreasing = T), ] %>% head()
+results.combos[order(results.combos$rmse), ] %>% head()
+
+# r2 = 0.733, rmse = 3.51
+# ncomp = 1, method = oscorepls
+
+
+saveRDS(results.combos, "./tuning/pls/beta/tune.RDS")
+results.combos <- results.combos %>%
+  mutate(ncomp = as.factor(ncomp), method = as.factor(method))
+ggplot(results.combos, aes(x = ncomp, y = method, fill = rsquared)) + 
+  geom_raster() + 
+  scale_fill_gradientn(colours = terrain.colors(20)) + 
+  theme_bw() + 
+  labs(title = "GLMNet tuning for beta-CD", 
+       x = "Number of components", y = "Maximum degrees of freedom", 
+       fill = "R2")
+ggsave("./tuning/pls/beta/tune.png", dpi = 450)
