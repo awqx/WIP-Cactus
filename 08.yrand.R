d79f60b1424c11c405eef6e068f1cd35b1d7dc88
awqx <alisonwxin@gmail.com>
2018-08-08 14:51:34 -0400

    Turning the 'Tropsha' step into the y-randomization step (because the Tropsha standards got absorbed into the model-building scirpts)

---
+++ 08.yrand.R
@@ -0,0 +1,9 @@
+# Models evaluated by standards from Tropsha 2010, DOI: 10.1002/minf201000061
+# "Best Practices for QSAR Model Development, Validation, and Exploitation"
+# Standards include y-randomization. 
+
+# According Rucker, Rucker, and Meringer in 'y-Randomization and its Variants in
+# QSAR/QSOR' (DOI: 10.1021/ci700157b), there are 5 modes of exploring the
+# viability of the model using different forms of permutation and randomization.
+# For the sake of time, I'll only be using Mode 1: scrambling (permuting) the
+# observed DelG (y) values while keeping descriptors the same
