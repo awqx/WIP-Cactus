edbbbdad0363f48e7fa4413f1d86876cb809d00a
awqx <awqx@users.noreply.github.com>
2020-01-18 13:46:53 -0500

    Random forest models
    Saved models for random forest.

--- 06-rforest.R
+++ 06-rforest.R
@@ -70,3 +70,87 @@ gamma_tune <- do.call(
 saveRDS(alpha_tune, "training/alpha/rf.RDS")
 saveRDS(beta_tune, "training/beta/rf.RDS")
 saveRDS(gamma_tune, "training/gamma/rf.RDS")
+
+# Model -------------------------------------------------------------------
+
+  # Alpha ----
+
+# refer to `05-svm.R` for comments and further explanation
+alpha_split <- readRDS("modeling-data/alpha/split.RDS")
+names(alpha_split) <- c(1:10)
+list(
+  "ntree" = 25, 
+  "nodesize" = 2, 
+  "mtry" = 1
+) %>%
+  train_rf(
+    folds = alpha_split, 
+    param = ., 
+    fold_avg = F
+) # best is 4
+
+# saving the best model
+trn <- readRDS("modeling-data/alpha/split.RDS")[[4]]$trn
+saveRDS(
+  randomForest(
+    x = trn[, -1:-2], 
+    y = trn$dG, 
+    ntree = 25, 
+    nodesize = 2, 
+    mtry = 1
+  ),
+  "model/alpha/rf.RDS"
+)
+
+  # Beta ----
+
+beta_split <- readRDS("modeling-data/beta/split.RDS")
+names(beta_split) <- c(1:10)
+list(
+  "ntree" = 25, 
+  "nodesize" = 5, 
+  "mtry" = 3
+) %>%
+  train_rf(
+    folds = beta_split, 
+    param = ., 
+    fold_avg = F
+) # best is 2
+trn <- readRDS("modeling-data/beta/split.RDS")[[2]]$trn
+saveRDS(
+  randomForest(
+    x = trn[, -1:-2], 
+    y = trn$dG, 
+    ntree = 25, 
+    nodesize = 2, 
+    mtry = 1
+  ),
+  "model/beta/rf.RDS"
+)
+
+  # Gamma ----
+
+gamma_split <- readRDS("modeling-data/gamma/split.RDS")
+names(gamma_split) <- c(1:10)
+list(
+  "ntree" = 25, 
+  "nodesize" = 10, 
+  "mtry" = 8
+) %>% 
+  train_rf(
+    folds = gamma_split, 
+    param = ., 
+    fold_avg = F
+  )
+# the best is split 8
+trn <- readRDS("modeling-data/gamma/split.RDS")[[8]]$trn
+saveRDS(
+  randomForest(
+    x = trn[, -1:-2], 
+    y = trn$dG, 
+    ntree = 25, 
+    nodesize = 10, 
+    mtry = 8
+  ),
+  "model/gamma/rf.RDS"
+)
