373fc26e7c092b5822db5b94a118ee266150f4af
awqx <awqx@users.noreply.github.com>
2020-01-23 10:02:06 -0500

    PLS model
    added tuned PLS models, removed old files

--- 08-pls.R
+++ 08-pls.R
@@ -92,3 +92,80 @@ gamma_tune <- do.call(
 saveRDS(alpha_tune, "training/alpha/pls.RDS")
 saveRDS(beta_tune, "training/beta/pls.RDS")
 saveRDS(gamma_tune, "training/gamma/pls.RDS")
+
+# Models ------------------------------------------------------------------
+
+  # Alpha ----
+
+alpha_split <- readRDS("modeling-data/alpha/split.RDS")
+names(alpha_split) <- c(1:10)
+list(
+  "ncomp" = 2, 
+  "kernel" = "simpls"
+) %>%
+  train_pls(
+    folds = alpha_split, 
+    param = ., 
+    fold_avg = F
+  ) # best is 10
+# saving the best model
+trn <- readRDS("modeling-data/alpha/split.RDS")[[10]]$trn
+saveRDS(
+  plsr(
+    dG~.,
+    data = trn[, -1],
+    ncomp = 2, 
+    kernel = "simpls"
+  ),
+  "model/alpha/pls.RDS"
+)
+
+  # Beta ----
+
+beta_split <- readRDS("modeling-data/beta/split.RDS")
+names(beta_split) <- c(1:10)
+list(
+  "ncomp" = 2, 
+  "kernel" = "simpls"
+) %>%
+  train_pls(
+    folds = beta_split, 
+    param = ., 
+    fold_avg = F
+  ) # best is 6
+# saving the best model
+trn <- readRDS("modeling-data/beta/split.RDS")[[6]]$trn
+saveRDS(
+  plsr(
+    dG~.,
+    data = trn[, -1],
+    ncomp = 2, 
+    kernel = "simpls"
+  ),
+  "model/beta/pls.RDS"
+)
+
+  # Gamma ----
+
+gamma_split <- readRDS("modeling-data/gamma/split.RDS")
+names(gamma_split) <- c(1:10)
+list(
+  "ncomp" = 1, 
+  "kernel" = "simpls"
+) %>%
+  train_pls(
+    folds = gamma_split, 
+    param = ., 
+    fold_avg = F
+  ) # best is 2
+# saving the best model
+trn <- readRDS("modeling-data/gamma/split.RDS")[[2]]$trn
+saveRDS(
+  plsr(
+    dG~.,
+    data = trn[, -1],
+    ncomp = 2, 
+    kernel = "simpls"
+  ),
+  "model/gamma/pls.RDS"
+)
